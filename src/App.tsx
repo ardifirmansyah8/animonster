939108348305da96ab57d2b20a5dbbcdb4df2a38
tifara.arisyah <tifara.arisyah@pinhome.id>
2023-09-12 18:46:11 +0700

    chore: init repo

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+const App = () => {
+  return (
+    <>
+      <div className="text-3xl font-bold underline text-teal-100">
+        Hello world!
+      </div>
+    </>
+  );
+};
+
+export default App;
