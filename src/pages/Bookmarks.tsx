ea9cd5c12b18351d90422cfd4b6591a66a223770
istidana <istidana.harjanti@rey.id>
2023-09-14 12:23:59 +0700

    feat: add bookmarks page

---
+++ src/pages/Bookmarks.tsx
@@ -0,0 +1,36 @@
+import { FC } from "react";
+
+import Layout from "../components/Layout";
+import Card from "../components/Card";
+import { BOOKMARK } from "../constants";
+import { Anime } from "../types";
+import { getItem } from "../utils";
+import { Link } from "react-router-dom";
+import { IconHome2 } from "@tabler/icons-react";
+
+const BookmarksPage: FC = () => {
+  const anime = getItem(BOOKMARK) as Anime[];
+
+  return (
+    <Layout>
+      <div className="px-8 mt-14">
+        <div className="flex gap-4 py-4">
+          <Link to={"/"} className="cursor-pointer text-blue-500">
+            <IconHome2 />
+          </Link>
+          <div>/</div>
+          <div>Bookmarks</div>
+        </div>
+        <div>
+          <div className="grid gap-y-6 2xl:grid-cols-6 xl:gap-x-8 lg:grid-cols-5 lg:gap-x-4">
+            {anime.map((media) => (
+              <Card key={media.id} data={media} isBookmark />
+            ))}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default BookmarksPage;
